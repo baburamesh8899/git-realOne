c044402a925bd767745286afe7be75c0d0f1d278
baburamesh8899 <baburamesh8899@gmail.com>
2015-03-25 17:27:13 +0530

    new js cha.

---
+++ html/typeof.js
@@ -0,0 +1,5 @@
+alert("typeof check");
+console.log(typeof true);console.log(typeof 2);
+console.log(typeof null);
+console.log(typeof String);
+console.log(typeof fun());
---
+++ js/typeof.js
@@ -0,0 +1,6 @@
+alert("typeof check");
+alert("typeof check");
+console.log(typeof true);console.log(typeof 2);
+console.log(typeof null);
+console.log(typeof String);
+console.log(typeof fun());
